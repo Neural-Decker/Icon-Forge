067d512c64ea8168d1b2b627dc76db07a3d2ed37
NF-Jason <norseforge.jason@outlook.com>
2026-06-04 14:00:15 +0200

    Add initial Icon Forge profile data assets

--- Assets/ClockworkPixieTools/IconForge/Runtime/Data/IconForgeProfile.cs
+++ Assets/ClockworkPixieTools/IconForge/Runtime/Data/IconForgeProfile.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
 
-public class IconForgeProfile : MonoBehaviour
+[CreateAssetMenu(
+    fileName = "IconForgeProfile",
+    menuName = "Clockwork Pixie/Icon Forge/Profile")]
+public class IconForgeProfile : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public IconOutputSettings outputSettings;
 }
